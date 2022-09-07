77ec2681a777a0b17cc6deccab733d8d8b7323aa
2k-akash <akashalwacitian@gmail.com>
2022-09-07 09:51:17 +0530

    completed

---
+++ week1/L.1.8Datatypes3.py
@@ -0,0 +1,14 @@
+a=int(5.7)
+b=int("10")
+print(a,type(a))
+print(b,type(b))
+
+c=float(7)
+d=float("5.1")
+print(c,type(c))
+print(d,type(d))
+
+e=str(9)
+f=str(5.3)
+print(e,type(e))
+print(f,type(f))
