d339b2956e869532bccfb561813b07c0510e0665
2k-akash <akashalwacitian@gmail.com>
2022-09-07 10:42:22 +0530

    completed

---
+++ week1/L1.10 OPERATORS AND EXPRESSIONS3.py
@@ -0,0 +1,14 @@
+print(5,5)
+print(8>7)
+print(4>=9)
+print(9<=9)
+print(9==9)
+print(60!=4)
+print(87!=87)
+print(True or False)
+print(False or True)
+print(True and False)
+print(not(True))
+print(not(False))
+
+
