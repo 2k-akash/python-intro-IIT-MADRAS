12495fae7f21d7abc883ce27a3f37aa5ea479d1f
2k-akash <akashalwacitian@gmail.com>
2022-09-10 20:17:10 +0530

    completed

---
+++ week2/L.2.5.Escape characters.py
@@ -0,0 +1,16 @@
+print('It\'s is a beautiful day')
+print('can\'t you get it')
+print('I\'m not well')
+
+print("It's a beautiful day")
+print("can't you get it")
+print("I'm not well")
+
+print("We are from \"IIT\" Madras")
+print('We are from \"IIT\" Madras')
+print("I am a \"cool\" guy")
+print('I am a \"cool\" guy')
+
+print('My name is akash. I am from tirunelveli')
+print('My name is akash.\nI am from tirunelveli')
+print('My name is akash.\t\t\tIam from pune.')
