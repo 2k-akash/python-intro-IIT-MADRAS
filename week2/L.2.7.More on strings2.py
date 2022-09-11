9d12898e6cfeae1abf556c9e0b1bd171280a3158
2k-akash <akashalwacitian@gmail.com>
2022-09-11 12:08:52 +0530

    completed

---
+++ week2/L.2.7.More on strings2.py
@@ -0,0 +1,13 @@
+alpha='abcdefghijklmnopqrstuvwxyz'
+s='mohammedrizwan'
+t=''
+i=1
+k=1
+t=t+(alpha[(((alpha.index(s[i]))+k)%26)])
+t=t+(alpha[(((alpha.index(s[i+1]))+k)%26)])
+t=t+(alpha[(((alpha.index(s[i+2]))+k)%26)])
+t=t+(alpha[(((alpha.index(s[i+3]))+k)%26)])
+t=t+(alpha[(((alpha.index(s[i+4]))+k)%26)])
+t=t+(alpha[(((alpha.index(s[i+5]))+k)%26)])
+t=t+(alpha[(((alpha.index(s[i+6]))+k)%26)])
+print(t)
