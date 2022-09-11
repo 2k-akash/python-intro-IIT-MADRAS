a7c89c159b6fe82b497a928556795373488569a7
2k-akash <akashalwacitian@gmail.com>
2022-09-11 10:22:58 +0530

    completed

---
+++ week2/L.2.6.String methods2.py
@@ -0,0 +1,20 @@
+x='python'
+y='String'
+z='PYTHON'
+v='Ponniyin Selvan'
+'''''
+print(x.islower())
+print(y.islower())
+print(z.islower())
+print(v.islower())
+
+print(x.isupper())
+print(y.isupper())
+print(z.isupper())
+print(v.isupper())
+'''''
+
+print(x.istitle())
+print(y.istitle())
+print(z.istitle())
+print(v.istitle())
