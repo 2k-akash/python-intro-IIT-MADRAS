529d23a9e1e010af934b67256cc0a82359b569bf
2k-akash <akashalwacitian@gmail.com>
2022-09-11 16:03:37 +0530

    completed

---
+++ week2/PPA01.py
@@ -0,0 +1,16 @@
+'''Accept a non zero integer as input.
+Print positive if it is greater than zero
+and negative if it is less than zero'''
+
+s=int(input())
+if(s!=0):
+    if(s>0):
+      print("positive")
+    else:
+        print("negative")
+else:
+    print(" ")
+
+
+
+
---
+++ week2/PPA02.py
@@ -0,0 +1,9 @@
+#piecewise function
+x=float(input())
+if(x>=10):
+    print((x**2)+2)
+elif(x>0 and x<10):
+    print(x+2)
+else:
+    print("otherwise")
+
