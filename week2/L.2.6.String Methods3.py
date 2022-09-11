7e538a1d193f2b0c08828c0af4f2a3a00af415be
2k-akash <akashalwacitian@gmail.com>
2022-09-11 10:36:03 +0530

    completed

---
+++ week2/L.2.6.String Methods3.py
@@ -0,0 +1,17 @@
+x='123'
+y='123abc'
+z='abc'
+#print(x.isdigit())
+#print(y.isdigit())
+#print(z.isdigit())
+#print(x.isalpha())
+#print(y.isalpha())
+#print(z.isalpha())
+
+
+
+print(x.isalnum())
+print(y.isalnum())
+print(z.isalnum())
+
+
