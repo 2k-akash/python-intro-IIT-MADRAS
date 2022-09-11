8ae368d459d301c78320e52ba3ee531f62a1aea6
2k-akash <akashalwacitian@gmail.com>
2022-09-11 15:18:56 +0530

    completed

---
+++ week2/L.2.10.Import Library.py
@@ -0,0 +1,45 @@
+'''import math
+
+print(math.log(10))
+print(math.sin(45))
+print(math.sin(90))
+print(math.sqrt(16))
+print(math.factorial(5))
+print(math.pow(10,3))
+import random
+print(random.random())
+
+#tossing using random function
+import random
+a=random.random()
+if(a<0.5):
+    print("heads")
+else:
+    print("tails")
+
+#tossing six faced dice
+import random
+print(random.randrange(1,7))'''
+
+#tossing two dice simultaneously
+import random
+dice1=random.randrange(1,7)
+dice2=random.randrange(1,7)
+total=dice1+dice2
+print("your pair of dice is",total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
