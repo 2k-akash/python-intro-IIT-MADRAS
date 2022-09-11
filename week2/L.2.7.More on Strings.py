ce249517bd2e70a6acf5f44183e0397790d350d1
2k-akash <akashalwacitian@gmail.com>
2022-09-11 11:37:21 +0530

    completed

---
+++ week2/L.2.7.More on Strings.py
@@ -0,0 +1,14 @@
+alpha='abcdefghijklmnopqrstuvwxyz'
+i=1000
+
+
+#print(alpha[i])
+#print(alpha[i+1])
+#print(alpha[i+2])
+#print(alpha[i+3])
+print(alpha[i%26])
+print(alpha[(i+1)%26])
+print(alpha[(i+2)%26])
+print(alpha[(i+3)%26])
+print(alpha[(i+4)%26])
+
