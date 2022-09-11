4805528496b65a3e2024e91a7629d668728d09ce
2k-akash <akashalwacitian@gmail.com>
2022-09-11 12:28:31 +0530

    completed

---
+++ week2/L.2.8.If Statement.py
@@ -0,0 +1,11 @@
+print("please,Enter your DOB:")
+birth_year=int(input())
+current_year=2022
+age=current_year-birth_year
+if(age<13):
+    print("you are under age.you can\'t watch this movie.")
+    print("wait until you are old enough to watch this movie.")
+else:
+    print('you are old enough to watch the "PONNIYIN SELVAN",enjoy!')
+    print("we think that this picture engages you")
+print("thank you")
