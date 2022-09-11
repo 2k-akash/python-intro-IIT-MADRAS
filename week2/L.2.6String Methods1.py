c877656003b8956926c67469c1aefe8f7a84194d
2k-akash <akashalwacitian@gmail.com>
2022-09-11 10:09:42 +0530

    completed

---
+++ week2/L.2.6String Methods1.py
@@ -0,0 +1,18 @@
+x='pytHoN StRInG'
+y='HaKUNa mAtAta'
+z='viKRAm vEDha'
+print(x.lower())
+print(y.lower())
+print(z.lower())
+print(x.upper())
+print(y.upper())
+print(z.upper())
+print(x.capitalize())
+print(y.capitalize())
+print(z.capitalize())
+print(x.title())
+print(y.title())
+print(z.title())
+print(x.swapcase())
+print(y.swapcase())
+print(z.swapcase())
