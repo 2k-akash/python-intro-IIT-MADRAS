07d5431902affbc8c80e66b02b9331dd68fb03d2
2k-akash <akashalwacitian@gmail.com>
2022-09-11 16:29:26 +0530

    completed

---
+++ week2/L.2.11.Diff Ways to Import Library.py
@@ -0,0 +1,12 @@
+'''import calendar
+print(calendar.month(2022,10))
+print(calendar.calendar(2022))
+from calendar import*
+print(month(2022,9))
+print(calendar(2022))
+from calendar import month
+print(month(2022,10))
+from calendar import calendar
+print(calendar(2021))'''
+from calendar import month as m
+print(m(2022,10))
