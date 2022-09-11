0881f3724ffd935556af2bf326f42e19c8854d25
2k-akash <akashalwacitian@gmail.com>
2022-09-11 11:09:33 +0530

    completed

---
+++ week2/L.2.4String Methods4.py
@@ -0,0 +1,16 @@
+
+''''x='-----------JULIUSCEASAR---------'
+print(x.strip('-'))
+print(x.lstrip('-'))
+print(x.rstrip('-'))
+print(x.strip('J'))'''
+x='IndianInstitueoftechnology'
+#print(x.startswith('I'))
+#print(x.startswith('i'))
+#print(x.endswith('y'))
+#print(x.endswith('Y'))'''
+#print(x.count("t"))
+#print(x.count('I'))
+print(x.index('t'))
+x=x.replace('t',"T")
+print(x)
