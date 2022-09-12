bd6b2dbf0e3f6b0517ac991e6f3aebad5511ccf6
2k-akash <akashalwacitian@gmail.com>
2022-09-12 18:16:06 +0530

    completed

---
+++ PRACTICE TESTS/PT1.1.py
@@ -0,0 +1,12 @@
+'''Print the following pattern.
+ There is exactly one space between any two consecutive numbers on any line.
+  There are no spaces at the end of any line.
+1 2 1
+1 2 3 2 1
+1 2 3 4 3 2 1
+1 2 3 4 5 4 3 2 1'''
+a=int(input())
+print(a,a+1,a)
+print(a,a+1,a+2,a+1,a)
+print(a,a+1,a+2,a+3,a+2,a+1,a)
+print(a,a+1,a+2,a+3,a+4,a+3,a+2,a+1,a)
