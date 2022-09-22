0e0ab6eb444d2dc352fac44c2040381f5b69368c
2k-akash <akashalwacitian@gmail.com>
2022-09-22 09:25:03 +0530

    completed

---
+++ week3/L.3.2.Intro to While loop.py
@@ -0,0 +1,14 @@
+print("When did India get its independence?")
+year=int(input())
+
+if(year==1947):
+    print("hip hip hurray.You got that right")
+else:
+    print("Comeon dont you know even this?")
+    print("Thats ok,I will let you attempt this once more")
+    print("When did India get its independence?")
+    year=int(input())
+    if (year==1947):
+        print("You got it!")
+    else:
+        print("Failed in your second attempt too?grrr..")
