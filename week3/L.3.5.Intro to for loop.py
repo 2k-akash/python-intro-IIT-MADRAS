fbe5c3445a0c938dfb55f55aa70207cba956a020
2k-akash <akashalwacitian@gmail.com>
2022-09-22 18:30:38 +0530

    completed

---
+++ week3/L.3.5.Intro to for loop.py
@@ -0,0 +1,25 @@
+'''
+for i in range(10):
+    #print("hello India")
+    #print(i,"Hello India")
+    print("*********")
+    print("# # # #
+
+for i in range(12):
+    if(i%2==0):
+        print(i,"Hello India")
+    else:
+        print(i,"hello world")
+'''
+n=int(input("Enter a number:"))
+m=int(input("ENTER a number:"))
+i=int(input("Enter a number:"))
+for i in range(n,m,i):
+    if(i%2==0):
+        print(i,"Hello world")
+    else:
+        print(i,"hiii")
+
+
+
+
