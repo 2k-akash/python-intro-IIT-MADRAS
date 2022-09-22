dd8defce99ebb3cf42d93f53feee05267a2c26be
2k-akash <akashalwacitian@gmail.com>
2022-09-22 18:51:19 +0530

    completed

---
+++ week3/L.3.7.For loop for multiplication tables.py
@@ -0,0 +1,11 @@
+'''
+for i in range(1,11):
+    print("2 X",i,"=",2*i)
+
+for i in range(1,11):
+    print('3 X',i,"=",3*i)
+'''
+n=int(input("Enter a number:"))
+for i in range(1,11):
+    print(n,'X',i,"=",n*i)
+
