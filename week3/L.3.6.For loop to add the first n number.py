3e264eeed19edc607e4f617b8453035e0d8ffb64
2k-akash <akashalwacitian@gmail.com>
2022-09-22 18:41:39 +0530

    completed

---
+++ week3/L.3.6.For loop to add the first n number.py
@@ -0,0 +1,12 @@
+'''
+ans=0
+for i in range(1,4):
+    ans=ans+i
+print("The answer is:",ans)
+'''
+print("Enter a no:")
+n=int(input())
+ans=0
+for i in range(n):
+    ans=ans+i
+print("The answer is:",ans)
