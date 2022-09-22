07eb274e9909e84d330aa79f97d0eb55a61ac2ad
2k-akash <akashalwacitian@gmail.com>
2022-09-22 19:24:31 +0530

    completed

---
+++ week3/L.3.8.More on range and for loop without range.py
@@ -0,0 +1,28 @@
+'''
+
+for x in range(1,11):
+    print(x)
+
+for x in range(1,11):
+    if(x%2==0):
+        print(x)
+
+for x in range(1,11,2):
+    print(x)
+
+for x in range(0,10,1):
+    print(x)
+
+for x in range(9,-1,-1):
+    print(x)
+
+for x in range(9,-1,-2):
+    print(x)
+
+country='India'
+for letter in country:
+    print(letter,end='')
+'''
+word='you are so beautiful'
+for letter in word:
+    print(letter,end = '')
