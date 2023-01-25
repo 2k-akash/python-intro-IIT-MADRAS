a14696006684bfc05174442244eae5c39e908c9c
2k-akash <akashalwacitian@gmail.com>
2023-01-25 23:40:57 +0530

    completed

---
+++ week1/PPA8.py
@@ -0,0 +1,7 @@
+#print the pattern
+a = int(input())
+print(a, a + 1, a)
+print(a, a + 1, a + 2, a + 1, a)
+print(a, a + 1, a + 2, a + 3, a + 2, a + 1, a)
+print(a, a + 1, a + 2, a + 3, a + 4, a + 3, a + 2, a + 1, a)
+
