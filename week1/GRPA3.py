51f04764a76c5104041f99430d9486ebc8870451
2k-akash <akashalwacitian@gmail.com>
2023-01-25 22:59:10 +0530

    completed

--- week1/GRPA3.py
+++ week1/GRPA3.py
@@ -1,10 +1,3 @@
 num=input()
-d1=int(num[0])
-d2=int(num[2])
-d3=int(num[4])
-d4=int(num[6])
-d5=int(num[8])
-
-dprod=d1*d2*d3*d4*d5
-print(dprod)
-
+prod=int(num[0])*int(num[2])*int(num[4])*int(num[6])*int(num[8])
+print(prod)
