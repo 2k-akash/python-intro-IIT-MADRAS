eb3f154f3c583b452854c15063fe7568ee112595
2k-akash <akashalwacitian@gmail.com>
2023-01-25 23:28:41 +0530

    completed

--- week1/PPA6.py
+++ week1/PPA6.py
@@ -1,9 +1,4 @@
-num=input()
-n1=int(num[0])
-n2=int(num[1])
-n3=int(num[2])
-n4=int(num[3])
-n5=int(num[4])
-
-nsum=n1+n2+n3+n4+n5
-print(nsum)
+#registration num
+num=str(input())
+st_code=num[0:2]
+print(st_code)
