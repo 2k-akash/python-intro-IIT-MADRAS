a268db209467cad8cdd88064f4d4d26d45800c0f
2k-akash <akashalwacitian@gmail.com>
2023-01-29 15:36:44 +0530

    completed

--- week2/PPA01.py
+++ week2/PPA01.py
@@ -1,16 +1,14 @@
 '''Accept a non zero integer as input.
 Print positive if it is greater than zero
 and negative if it is less than zero'''
-
-s=int(input())
-if(s!=0):
-    if(s>0):
-      print("positive")
+a=int(input())
+if (a!=0):
+    if a>0:
+        print("positive")
     else:
         print("negative")
 else:
     print(" ")
-
 
 
 
