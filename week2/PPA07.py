259ccca266c7c5b59dd7ee2d0cd355c65c8b3e3e
2k-akash <akashalwacitian@gmail.com>
2023-02-02 22:53:17 +0530

    completed

--- week2/PPA07.py
+++ week2/PPA07.py
@@ -3,7 +3,14 @@ b = input()
 c = input()
 d = input()
 e = input()
-if a in b and b in c and c in d and d in e:
+'''if a in b and b in c and c in d and d in e:
     print("magical")
 else:
-    print("non-magical")
+    print("non-magical")'''
+flag = "non-magical"
+if a in b:
+    if b in c:
+        if c in d:
+            if d in e:
+                flag = "magical"
+print(flag)
