deaa7654f1ae729969f2cf1d59b824b0d3fce6ae
2k-akash <akashalwacitian@gmail.com>
2023-02-02 18:39:21 +0530

    completed

--- week2/PPA03.py
+++ week2/PPA03.py
@@ -1,16 +1,14 @@
 #print the time of the day
 T=int(input())
-if T<0:
+if (T<0 or T>=24):
     print("INVALID")
-elif 0<=T<=5:
+elif (0<=T<=5):
     print("NIGHT")
-elif 6<=T<=11:
+elif (6<=T<=11):
     print("MORNING")
-elif 12<=T<=17:
+elif (12<=T<=17):
     print("AFTERNOON")
-elif 18<=T<=23:
-    print("EVENING")
 else:
-    print("INVALID")
+    print("EVENING")
 
 
