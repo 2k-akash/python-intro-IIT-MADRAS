9d1169253da1d5e31810f8fb6200dccdd50710d5
2k-akash <akashalwacitian@gmail.com>
2023-02-05 19:04:30 +0530

    completed

--- week2/PPA13.py
+++ week2/PPA13.py
@@ -4,7 +4,7 @@ total_A_runs = A_1_runs + A_2_runs
 total_B_runs = B_1_runs + B_2_runs
 if (total_B_runs > total_A_runs):
     print("Team B")
-if (total_A_runs > total_B_runs and B_2_wickets == 10):
+elif (total_A_runs > total_B_runs and B_2_wickets == 10):
     print('Team A')
 else:
     print('DRAW')
