1b6eba7cfe871eb2d80e1779bb51df03fcdd27f5
2k-akash <akashalwacitian@gmail.com>
2023-02-08 20:56:15 +0530

    completed

---
+++ week3/L.3.9.3.Formatted String.py
@@ -0,0 +1,10 @@
+#pi
+'''
+pi = 22/7
+print(f'the value of PI={pi}')
+print('the value of PI={0}'.format(pi))
+print('value of PI= %f'%(pi))'''
+print('{0:5d}'.format(1))
+print('{0:5d}'.format(11))
+print('{0:5d}'.format(111))
+
