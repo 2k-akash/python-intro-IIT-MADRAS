f85dc8e8b7fd223574a5e09b97427ae2d5164115
2k-akash <akashalwacitian@gmail.com>
2023-02-08 19:52:44 +0530

    completed

---
+++ week3/L.3.9.1.Formatted String.py
@@ -0,0 +1,13 @@
+'''
+for x in range(10):
+    print(x,end="   ")'''
+
+#print the date
+d = int(input())
+m = int(input())
+y = int(input())
+#print("Todays date is:",d,m,y,sep ="/")
+print("Todays date is",end =" ")
+print(d,m,y,sep ="/")
+
+
