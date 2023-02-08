2cd8299ed9638fbdba12fb6bcf7f3205dbcbc279
2k-akash <akashalwacitian@gmail.com>
2023-02-08 17:27:57 +0530

    completed

--- week3/L.3.4.2.Find the no of digits in the given no.py
+++ week3/L.3.4.2.Find the no of digits in the given no.py
@@ -1,6 +1,7 @@
+#find the no of digits
 num = abs(int(input("Enter a no:")))
 digits = 1
-while(num >9):
+while(num > 9):
     num = num//10
-    digits = digits +1
+    digits = digits + 1
 print(digits)
