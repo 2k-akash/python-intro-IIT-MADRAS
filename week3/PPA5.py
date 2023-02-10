3a7eda8343c71ce1c25231c561ac4c2597f9a9aa
2k-akash <akashalwacitian@gmail.com>
2023-02-11 00:05:33 +0530

    completed

--- week3/PPA5.py
+++ week3/PPA5.py
@@ -1,8 +1,8 @@
-n=int(input())
-max=0
-
-while n!=0:
-    if max < n:
-        max=n
-    n=int(input())
-print(max)
+#To find the max_val
+max_val = 0
+n = -1
+while(n != 0):
+    n = int(input())
+    if (n >= max_val):
+        max_val = n
+print(max_val)
