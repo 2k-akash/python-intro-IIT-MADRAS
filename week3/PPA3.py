641e7a3415692c34bcf067465ddc131c8a9531ea
2k-akash <akashalwacitian@gmail.com>
2023-02-10 23:37:26 +0530

    completed

--- week3/PPA3.py
+++ week3/PPA3.py
@@ -1,7 +1,7 @@
-a=int(input())
-b=int(input())
-sum=0
-for i in range(1000,2001):
-    if (i % a == 0 and i % b == 0):
-        sum=sum + i
+a = abs(int(input()))
+b = abs(int(input()))
+sum = 0
+for i in range(1000,2001): #range[1000,2000]
+    if( i%a == 0 and i%b == 0):#the points that are divisible by both a and b
+        sum = sum + i
 print(sum)
