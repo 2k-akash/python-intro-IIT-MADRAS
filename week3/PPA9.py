de48a4cc12e9678da9526ccff006ed1112ed0bf9
2k-akash <akashalwacitian@gmail.com>
2023-02-11 01:28:26 +0530

    completed

--- week3/PPA9.py
+++ week3/PPA9.py
@@ -1,5 +1,11 @@
-n=int(input())
+#Accept a positive integer n as input
+#print triangle of zeros for n lines
+'''n=int(input())
 
 for i in range(n+1):
-    print("0"* i)
-print()
+    print("0"* i)'''
+num = int(input())
+for i in range(num):
+    for j in range(i+1):
+        print(str(0),end="")
+    print("")
