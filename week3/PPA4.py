1489e94131e864da91727b4670d9d64121860955
2k-akash <akashalwacitian@gmail.com>
2023-02-10 23:51:28 +0530

    completed

--- week3/PPA4.py
+++ week3/PPA4.py
@@ -1,12 +1,10 @@
-n=int(input())
-
-flag=True
-for i in range(2,n):
+n = int(input())
+count = 0
+for i in range(1, n+1):
     if (n % i == 0):
-        flag= False
-
-if flag:
-    print("PRIME")
-else:
+        count = count + 1
+if (count > 2):
     print("NOTPRIME")
+else:
+    print("PRIME")
 
