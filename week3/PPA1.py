2c58b6e0d4be0b061472af0b3cf2d7e5f8fd026c
2k-akash <akashalwacitian@gmail.com>
2023-02-10 23:17:33 +0530

    completed

--- week3/PPA1.py
+++ week3/PPA1.py
@@ -1,6 +1,8 @@
 #Accept a positive integer n as input.
 # print the first n positive integers,
 # one number on each line.
-n=int(input())
-for i in range(1,n+1):
+
+
+n = int(input())
+for i in range(1, n + 1):
     print(i)
