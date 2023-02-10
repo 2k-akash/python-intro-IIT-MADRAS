207dbd83eb6b09ab08e11960ffa5388c41a583a2
2k-akash <akashalwacitian@gmail.com>
2023-02-11 01:07:49 +0530

    completed

--- week3/PPA8.py
+++ week3/PPA8.py
@@ -1,6 +1,8 @@
-n=int(input())
-for i in range(1,n+1):
-    if i != n:
-        print(i,end = ",")
+#Accept a positive integer n as input
+#print the first n integers on a line separated by a comma
+num = int(input())
+for i in range(1, num+1):
+    if (i != num):
+        print(i,end=",")
     else:
         print(i)
