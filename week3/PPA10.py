b5181e4b45476862b565df2ae3e49bc88e9f01d0
2k-akash <akashalwacitian@gmail.com>
2023-02-11 20:21:54 +0530

    completed

--- week3/PPA10.py
+++ week3/PPA10.py
@@ -1,13 +1,13 @@
-n=int(input())
-sum = 0
-prime = True
-for i in range(2,n+1):
-    for j in range(2,i):
-        if i % j == 0:
-            prime = False
-            break
-    if prime:
-        sum+= i
-    prime = True
+#Accept positive integer as input and print the sum of all prime numbers in the range[1,n]
+#if thre are no prime numbers print 0
 
+n = int(input())
+sum = 0
+for i in range(1, n+1):
+    count = 0
+    for j in range(1,i+1):
+        if (i % j == 0):
+            count = count +1
+    if (count == 2):
+        sum = sum + i
 print(sum)
