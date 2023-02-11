ee19e24eda0e27ba2ae52d2c55e7ab3fc7a43a3b
2k-akash <akashalwacitian@gmail.com>
2023-02-11 22:25:11 +0530

    completed

---
+++ week3/PPA15.py
@@ -0,0 +1,21 @@
+'''
+Accept a string as input,print Integer if the string is an integer,print Float if it a float,else print None
+
+'''
+n = input()
+chars ='0123456789.'
+
+isstring = False
+
+for i in n:
+    if i not in chars:
+        isstring = True
+if not isstring:
+    if '.' in n and n.count('.') == 1:
+        print('Float')
+    elif (n.count('.') == 0):
+        print('Integer')
+    else:
+        print('None')
+else:
+    print('None')
