3a5db86e60670fb7e550076bde207131797474f7
2k-akash <akashalwacitian@gmail.com>
2023-02-12 19:55:23 +0530

    completed

--- week3/GRPA02.py
+++ week3/GRPA02.py
@@ -1,10 +1,11 @@
-n=int(input())
+#print all the prime factors of n in ascending order
 
-for i in range(2,n+1):
-    if n % i == 0:
+n = int(input())
+for i in range(2, n+1):
+    if (n % i == 0):
         prime = True
-        for j in range(2,i):
-            if i % j == 0:
-                prime =False
+        for j in range(2, i):
+            if (i%j == 0):
+                prime = False
         if prime:
             print(i)
