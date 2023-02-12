095bb397bc864f272773a49858cdaecc1adc5ee6
2k-akash <akashalwacitian@gmail.com>
2023-02-12 20:21:17 +0530

    completed

--- week3/GRPA04.py
+++ week3/GRPA04.py
@@ -1,11 +1,12 @@
-import string
+#Accept a string as input, convert it to lower case,
+#sort the string in alphabetical order,and print the sorted string to the console
 
 s = input().lower()
-t = string.ascii_lowercase
-o = ""
-for i in t:
-    for j in s:
-        if i == j:
-            o += j
 
-print(o)
+a = 'abcdefghijklmnopqrstuvwxyz'
+t = ''
+for x in a:
+    for y in s:
+        if (x == y):
+            t +=y
+print(t)
