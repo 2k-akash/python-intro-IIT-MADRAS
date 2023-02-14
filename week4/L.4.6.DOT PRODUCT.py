e0a1e711c220f2e443e9eaa600b7f0c694c186a4
2k-akash <akashalwacitian@gmail.com>
2023-02-14 16:18:59 +0530

    completed

---
+++ week4/L.4.6.DOT PRODUCT.py
@@ -0,0 +1,14 @@
+#sum
+l = [1,3,4,67,18,17]
+sum = 0
+for i in range((len(l))):
+    sum = sum + l[i]
+print(sum)
+
+#product
+x = [1,7,4,3,2,1]
+y = [3,2,4,7,9,0]
+sum = 0
+for i in range(len(l)):
+    sum = sum + x[i]*y[i]
+print(sum)
