a8f974b04aae5d48cf85014960d4bf54359cf23a
2k-akash <akashalwacitian@gmail.com>
2023-02-21 22:13:11 +0530

    completed

---
+++ week4/PPA2.py
@@ -0,0 +1,14 @@
+'''
+Accept a sequence of words as input, append all these words to a list in the order in which they are entered,
+and print this list as output.
+The first line in the input is a positive integer
+n that denotes the number of words in the sequence. The next
+n lines will have one word on each line.
+'''
+
+n = int(input())
+L = []
+for i in range(n):
+    word = input()
+    L.append(word)
+print(L)
