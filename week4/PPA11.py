6b8447300b6474b0b0e6eaebcfce9762df911648
2k-akash <akashalwacitian@gmail.com>
2023-02-22 18:07:35 +0530

    completed

---
+++ week4/PPA11.py
@@ -0,0 +1,13 @@
+
+def solution (L):
+    elem = 0
+    sorted_L = []
+    while L != []:
+        max_elem = L[0]
+        if (elem >= max_elem):
+            max_elem = elem
+    L.remove(max_elem)
+    sorted_L.append(max_elem)
+    return sorted_L
+
+
