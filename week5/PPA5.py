66b6efd884c9e496b09e219370b5003324a60438
2k-akash <akashalwacitian@gmail.com>
2023-03-05 18:14:06 +0530

    completed

---
+++ week5/PPA5.py
@@ -0,0 +1,14 @@
+def first_three(L):
+    fmax = -10000
+    smax = -10000
+    tmax = -10000
+    for i in range(len(L)):
+        if L[i] > fmax:
+            fmax, smax, tmax = L[i],fmax, smax
+        elif L[i] > smax:
+            smax,tmax = L[i],smax
+        elif L[i] > tmax:
+            tmax = L[i]
+    return fmax,smax,tmax
+L = [1,45,67,2,34,5,233]
+print(*first_three(L))
