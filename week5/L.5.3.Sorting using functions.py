ecc5df2904523b9f3ada24de53973155466295c5
2k-akash <akashalwacitian@gmail.com>
2023-03-05 11:29:17 +0530

    completed

--- week5/L.5.3.Sorting using functions.py
+++ week5/L.5.3.Sorting using functions.py
@@ -1,3 +1,4 @@
+#To sort the function
 def obvious_sort(l):
     x = []
     while (len(l) > 0):
@@ -9,4 +10,20 @@ def obvious_sort(l):
         l.remove(mini)
     return x
 l = [90,23,97,88,5,1]
+print(obvious_sort(l))
+
+def min_list(l):
+    mini = l[0]
+    for i in range(len(l)):
+        if l[i] < mini :
+            mini = l[i]
+    return mini
+def obvious_sort(l):
+    x = []
+    while(len(l) > 0):
+        mini = min_list(l)
+        x.append(mini)
+        l.remove(mini)
+    return x
+l= [12,45,678,90]
 print(obvious_sort(l))
