7dbf6d6a6c0100aa4428380a6fe9f0e4ff6f8f7e
2k-akash <akashalwacitian@gmail.com>
2023-03-05 23:33:38 +0530

    completed

--- week5/GRPA3.py
+++ week5/GRPA3.py
@@ -7,8 +7,13 @@ def distance(word_1, word_2):
         return -1
     letters = 'abcdefghijklmnopqrstuvwxyz'
     size = len(word_1)
+    dist = 0
     for i in range(size):
         c1, c2 = word_1[i], word_2[i]
         d = abs(letters.index(c1) - letters.index(c2))
         dist = dist + d
     return dist
+word_1 = 'akash'
+word_2 = 'suraj'
+print(distance(word_1,word_2))
+
