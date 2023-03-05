00671a58f12029c86b3064dfdcfac640394ebba0
2k-akash <akashalwacitian@gmail.com>
2023-03-05 11:23:35 +0530

    completed

---
+++ week5/L.5.3.Sorting using functions.py
@@ -0,0 +1,12 @@
+def obvious_sort(l):
+    x = []
+    while (len(l) > 0):
+        mini = l[0]
+        for i in range(len(l)):
+            if l[i] < mini:
+                mini = l[i]
+        x.append(mini)
+        l.remove(mini)
+    return x
+l = [90,23,97,88,5,1]
+print(obvious_sort(l))
