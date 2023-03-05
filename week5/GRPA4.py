cbe39c7eaa05a13c8481fbdb45c53438b3e524fe
2k-akash <akashalwacitian@gmail.com>
2023-03-05 23:46:25 +0530

    completed

---
+++ week5/GRPA4.py
@@ -0,0 +1,14 @@
+def is_magic(mat):
+    m = len(mat)
+    d1sum, d2sum = 0,0
+    for i in range(m):
+        d1sum +=mat[i][i]
+        d2sum +=mat[i][m-i-1]
+    if not(d1sum == d2sum):
+        return 'NO'
+    for i in range(m):
+        rsum, csum = 0,0
+        for j in range(m):
+            rsum +=mat[i][j]
+            csum +=mat[j][i]
+        if not (rsum == csum == dsum)
