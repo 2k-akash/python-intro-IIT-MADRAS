483a73625526335b9fc44a51dbad3cb83d85fc2b
2k-akash <akashalwacitian@gmail.com>
2023-03-06 18:24:36 +0530

    completed

---
+++ week6/L.6.1.2.Lists and sets.py
@@ -0,0 +1,14 @@
+#dictionaries
+'''d = {}
+d['sudharshan'] = 98989898
+d['ramya'] = 7878787
+d['ravi'] = 987899
+print(d)'''
+
+l = [1,2,3,5,7,9]
+for i in range(len(l)):
+    print(i)
+for x in l:
+    print(x)
+for i in range(len(l)):
+    print(l[i])
