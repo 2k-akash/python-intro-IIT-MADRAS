e050b93f44b40d8d240cf63424218ba83695438c
2k-akash <akashalwacitian@gmail.com>
2023-03-06 19:41:29 +0530

    completed

---
+++ week6/L.6.3.Tuples.py
@@ -0,0 +1,17 @@
+t = (2,7,8,9,64,101,108,65)
+print(t)
+
+
+t = tuple(range(10))
+print(t)
+
+
+import string
+print(string.ascii_letters)
+print(string.ascii_uppercase)
+print(string.ascii_lowercase)
+
+s = string.ascii_letters
+print(s)
+t = set(s)
+print(t)
