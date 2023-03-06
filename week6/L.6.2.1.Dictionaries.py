43821b71c6417f9ae6fb51f019285c551f2dcd26
2k-akash <akashalwacitian@gmail.com>
2023-03-06 19:02:27 +0530

    completed

---
+++ week6/L.6.2.1.Dictionaries.py
@@ -0,0 +1,9 @@
+d = {}
+d['sudharsan'] = 9898789
+d['ramya']  =676729
+d['ravi'] = 8777
+print(d)
+
+d = {'sudharsan' :[93,89,78],'ajit' :[73,64,28],'supriya':[81,66,90]}
+print(d['sudharsan'][0])
+print(d['sudharsan'][1])
