c16c02cd8705557213e1e6d095d64bd22cd7675f
2k-akash <akashalwacitian@gmail.com>
2023-03-10 16:22:54 +0530

    completed

--- week4/PPA11.py
+++ week4/PPA11.py
@@ -1,13 +1,14 @@
 # sort the list using function
 def solution (L):
-    elem = 0
     sorted_L = []
     while L != []:
         max_elem = L[0]
-        if (elem >= max_elem):
-            max_elem = elem
-    L.remove(max_elem)
-    sorted_L.append(max_elem)
+        for elem in L:
+            if (elem >= max_elem):
+                max_elem = elem
+        L.remove(max_elem)
+        sorted_L.append(max_elem)
     return sorted_L
+
 
 
