b7c984dfc66a2a3cb4154d883d87bbb8453cfc4a
2k-akash <akashalwacitian@gmail.com>
2023-03-10 23:15:22 +0530

    completed

---
+++ week5/PPA9.py
@@ -0,0 +1,11 @@
+def get_column(mat,col):
+    l = []
+    for i in range(len(mat)):
+        l.append(mat[i][col])
+    return l
+
+def get_row(mat,row):
+    l = []
+    for i in range(len(mat)):
+        l.append(mat[row][i])
+    return l
